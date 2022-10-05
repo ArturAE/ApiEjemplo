9c64839fa549227be3dc6472f56296b2ea2ba8ba
ArturAE <luisarturo98ae@gmail.com>
2022-10-04 21:42:52 -0500

    configuracion ssl

--- config/db.js
+++ config/db.js
@@ -1,5 +1,14 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env['DATABASE_URL']);
+
+//al postgres ya sea local o de heroku o donde sea
+const sequelize = new Sequelize(process.env['DATABASE_URL'], {
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+});
 
 module.exports = sequelize;
